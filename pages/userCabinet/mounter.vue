f22610cced396c1ea2de9e54cc1944e71cbe6812
monkriz1991 <monkriz1991@gmail.com>
2021-06-30 20:38:10 +0300

    mount

--- pages/userCabinet/mounter.vue
+++ pages/userCabinet/mounter.vue
@@ -4,6 +4,7 @@
         <h1 class="h1-user-cab">Страница монтажника</h1>
         <Form/>
         <AddGalery/>
+        <Project/>
     </div>
 </template>
 
@@ -11,11 +12,13 @@
 import NavbarCabinet from '~/components/cabinet/NavbarCabinet.vue'
 import Form from '~/components/cabinet/mounter/Form.vue'
 import AddGalery from '~/components/cabinet/mounter/AddGalery.vue'
+import Project from '~/components/cabinet/mounter/Project.vue'
 export default {
     components:{
         NavbarCabinet,
         Form,
-        AddGalery
+        AddGalery,
+        Project
     },
     data() {
      return{
