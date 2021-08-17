3a58df836fd2fbef422ec9c670f989c8d5dedc24
svalench <chitsalex@gmail.com>
2021-08-17 22:23:43 +0300

    FIX card close all and delete card position

--- dist/_nuxt/f431393.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{681:function(n,t,e){"use strict";e.r(t);var r={data:function(){return{}}},c=e(5),component=Object(c.a)(r,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",[n._v("\n    123\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
