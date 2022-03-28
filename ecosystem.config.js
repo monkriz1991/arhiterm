d6224be1071d6b928faea2f5f0c63872134774f3
svalench <chitsalex@gmail.com>
2022-03-28 19:58:38 +0300

    merge

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'Arhiterm.by',
+      name: 'Arhiterm',
       exec_mode: 'cluster',
       instances: 'max', //
       script: './node_modules/nuxt/bin/nuxt.js',
