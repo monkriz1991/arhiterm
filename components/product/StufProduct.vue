4ab0380d2de6d3bd21c4a6f1e83287ece88a7c79
monkriz1991 <monkriz1991@gmail.com>
2022-12-05 20:32:44 +0300

    stuf

---
+++ components/product/StufProduct.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="index-top">
+    <h2>С этим товаром покупают</h2>
+        <el-row style="margin-left:-10px;margin-right:-10px;">
+            <hooper :settings="hooperSettings">
+                <slide
+                v-for="item in stufProduct" :key="item.id"
+                >
+                    <nuxt-link :to="{ name: 'product-product', params: {product:`${item.kirilica_name}`,id:`${item.kirilica_name}`} }" class="nuxt-link-cat">
+                        <div class="index-top-block">
+                            <div class="index-top-block-img">
+                                <nuxt-img
+                                provider="twicpics"
+                                :src="item.img.substring(24)" 
+                                :alt="item.name"
+                                format="webp"
+                                quality="50"
+                                sizes="sm:50vw md:50vw lg:50px" 
+                                loading="lazy" 
+                                />
+                            </div> 
+                            <div class="index-top-block-desc">
+                                <strong>
+                                    {{item.manufacturer_name}}
+                                </strong>
+                                <span v-if="item.name.length<50">{{item.name}}</span>
+                                <span v-else>{{item.name.substring(0,50)+".." }}</span>
+                            </div>
+                            <div class="index-top-cost">
+                                <b>от</b><strong
+                                v-for="(item_product,idx) in item.product" :key="idx"
+                                v-show="idx==0"
+                                >{{item_product.price}}</strong> 
+                                <span>руб/{{item.units}}</span>
+                            </div>
+                        </div>
+                    </nuxt-link>
+                </slide>
+                <hooper-navigation slot="hooper-addons"></hooper-navigation>
+            </hooper>
+        </el-row>
+    </div>
+</template>
+
+
+<script>
+
+import { Hooper, Slide,
+Pagination as HooperPagination,
+Navigation  as HooperNavigation} from 'hooper';
+import 'hooper/dist/hooper.css';
+export default ({
+    props:['stufProduct'],
+    components: {
+        Hooper,
+        Slide,
+        HooperPagination,
+        HooperNavigation
+    },
+    data() {
+        return {
+            hooperSettings: {
+                infiniteScroll: false,
+                wheelControl:false,
+                mouseDrag:false,
+                loop:false,
+                breakpoints: {
+                    1800: {
+                        itemsToShow: 3,
+                    },
+                    1500: {
+                        itemsToShow: 3
+                    },
+                    900: {
+                        itemsToShow: 1
+                    },
+                    360: {
+                        itemsToShow: 1
+                    },
+                }
+            }
+        }
+    },
+    computed:{
+    },
+    mounted(){
+    },
+    methods:{
+    }
+})
+</script>
--- store/tovar.js
+++ store/tovar.js
@@ -2,29 +2,43 @@
 //const sleep = m => new Promise(r => setTimeout(r, m))
 export const state = () =>({
     productItem: [],
+    stufProduct:[],
+
 })
 
 export const mutations = {
     setProductItem (state,productItem) {
         state.productItem = productItem
     },
+    setstufProduct (state,stufProduct) {
+        state.stufProduct = stufProduct
+    },
+    setstufProductNull(state,stufProduct) {
+        state.stufProduct = []
+    },
 }
 
 export const actions = {
-    /*
-     *  записывает в store
-     */
     async getProductItem ({ commit},id) {
         // await sleep(50)
-       // let product = await this.$axios.$get(`api/get/product?kirilica=${id}`); 
+        let stuf = []
+        commit('setstufProductNull')
         let product = await this.$axios.$get(`api/get/product?kirilica=${id}`); 
         commit('setProductItem', product)
+        if(product.popularly.length){
+            for(let i in product.popularly){
+                let productstuf = await this.$axios.$get(`catalog/product/${product.popularly[i]}/`);
+                stuf.push(productstuf);
+            }
+            commit('setstufProduct',stuf)
+        }
         return product
     }
 }
 
 export const getters = {
     productItem: state => state.productItem,
+    stufProduct: state => state.stufProduct,
     
 }
 
