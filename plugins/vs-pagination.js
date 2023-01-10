ed4de3a9c2aac10338fda43da62badc91a4b0b14
monkriz1991 <monkriz1991@gmail.com>
2023-01-10 21:45:38 +0300

    paginated

---
+++ plugins/vs-pagination.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VsPagination from '@vuesimple/vs-pagination';
+
+Vue.component('vs-pagination', VsPagination);
--- plugins/vue-sliding-pagination.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import SlidingPagination from 'vue-sliding-pagination'
-
-if (process.browser) {
-Vue.use(SlidingPagination);
-}
