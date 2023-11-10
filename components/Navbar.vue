dbabfbe6b2de6eeff26c61d93596100b11958d0e
krizis <monkriz1991@gmail.com>
2023-11-10 15:12:05 +0300

    gr

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -369,15 +369,16 @@ export default {
         return a.position - b.position;
       });
     },
-    //   open2() {
-    //   this.$notify({
-    //     dangerouslyUseHTMLString: true,
-    //     title: 'Уважаемые клиенты!',
-    //     message: '<strong>29.04.23 график работы</strong><br> склад до <strong>13.00</strong><br>офис до <strong>14.00</strong>',
-    //     type: 'warning',
-    //     duration:50000,
-    //   });
-    // },
+    open2() {
+      this.$notify({
+        dangerouslyUseHTMLString: true,
+        title: "Уважаемые клиенты!",
+        message:
+          "<strong>11.11.23 график работы </strong><br>офис и склад до <strong>15.30</strong>",
+        type: "warning",
+        duration: 9000,
+      });
+    },
   },
 
   mounted() {
@@ -395,7 +396,7 @@ export default {
       this.headerTop = this.$refs.header.getBoundingClientRect().top;
     });
 
-    // this.open2()
+    this.open2();
   },
   watch: {
     scrollY(newValue) {
